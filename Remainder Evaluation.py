00fa0ff6178dee3d0120c01cbc34c63454a4c9de
Sanya Gera <75986201+SanyaGera@users.noreply.github.com>
2021-06-27 10:41:16 -0700

    Create Remainder Evaluation.py

---
+++ Remainder Evaluation.py
@@ -0,0 +1,6 @@
+#Given two positive integers num1 and num2, the task is to find the remainder when num1 is divided by num2.
+#Solution
+num1=int(input("Enter your first number"))
+num2=int(input("Enter your second number"))
+remainder = num1%num2
+print(remainder)
