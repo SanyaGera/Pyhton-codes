5c9714ff1e709d4a184d722a408ab2d80cc4cf10
Sanya Gera <75986201+SanyaGera@users.noreply.github.com>
2021-06-30 08:00:54 -0700

    Create Product.py

---
+++ Product.py
@@ -0,0 +1,6 @@
+#Given two integers A and B. Your task is to return their product.
+#Solution
+A=int(input("Enter your first value"))
+B=int(input("Enter your second value"))
+Product=A*B
+print("Product :", Product)
