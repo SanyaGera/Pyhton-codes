45720d6c7652955d702ab6327525b1519f7844c4
Sanya Gera <75986201+SanyaGera@users.noreply.github.com>
2021-06-30 08:18:24 -0700

    Create Addition.py

---
+++ Addition.py
@@ -0,0 +1,6 @@
+#Given two numbers A and B. Your task is to return the sum of A and B.
+#Solution
+A=int(input("Enter your first value"))
+B=int(input("Enter your second value"))
+Product=A+B
+print("Product :", Product)
