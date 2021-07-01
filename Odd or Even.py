2f95fd4ce5b2efaf85354c5f1e31f3279588dfc4
Sanya Gera <75986201+SanyaGera@users.noreply.github.com>
2021-07-01 10:46:18 -0700

    Create Odd or Even.py

---
+++ Odd or Even.py
@@ -0,0 +1,7 @@
+#Given a positive integer N, determine whether it is odd or even.
+#Solution
+n=int(input("Enter the required number"))
+if n%2==0:
+    print("The number is even")
+else:
+    print("The number is odd")
