60cc02ad4ac74d08bdf05319dc11fa7cbb0ba8b3
Sanya Gera <75986201+SanyaGera@users.noreply.github.com>
2021-07-03 07:28:11 -0700

    Create Armstrong Numbers.py

---
+++ Armstrong Numbers.py
@@ -0,0 +1,16 @@
+#For a given 3 digit number, find whether it is armstrong number or not. An Armstrong number of three digits is an integer such that the sum of the cubes of its
+#digits is equal to the number itself. Return "Yes" if it is a armstrong number else return "No".
+#NOTE: 371 is an Armstrong number since 33 + 73 + 13 = 371
+#Solution
+N=int(input("Enter your 3 digit number"))
+Sum=0
+S=str(N) #Convert the integer into string
+for i in range(len(S)):
+    j=int(S[i])
+    Sum=Sum+j**3
+
+if Sum==N:
+    print("YES")
+
+else:
+    print("NO")
